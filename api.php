1f949a8c1c7fa87a013b926689247ffd0bb21c32
sarahmonks <sarah.monks@webelevate.ie>
2019-01-17 18:12:41 +0000

    fixed formatting

--- api.php
+++ api.php
@@ -6,12 +6,11 @@ if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
     $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
 }
 $ErrorHandler = new ErrorHandler("api");
-       
 try {
     $API = new SocialNetworkAPI($_REQUEST, $_SERVER['HTTP_ORIGIN']);
     echo $API->processAPI();
 } catch (Exception $e) {
-	//create a log entry to record the error message
+    //create a log entry to record the error message
     $ErrorHandler->createLogEntry("api", $e->getMessage());
     echo json_encode(array('responseStatus' => 'error', 'errorMessage' => $e->getMessage()));
 }
