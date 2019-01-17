67a2270ae3d37c4da733595d640846ba509e0d45
sarahmonks <sarah.monks@webelevate.ie>
2019-01-17 17:41:52 +0000

    added comments and changed the commentsData array to a local variable

--- Comment.class.php
+++ Comment.class.php
@@ -1,14 +1,15 @@
 <?php
 /*
- * A class for working with Comment data (related to posts).
+ * A class for working with Comment data (related to Posts).
  */
 class Comment{
 	
-  private $DatabaseConnection;// stores the database handler
+  private $DatabaseConnection;//stores the database handler
   private $ErrorHandler;
   private $Validator;
+  //we use protected so the properties can be accessed and overidden if necessary
   protected $commentID; 
-  protected $commentTime;    // using protected so they can be accessed and overidden if necessary
+  protected $commentTime;    
   protected $commentText; 
   protected $commentUserID; 
   protected $postID; //each comment will have a postID which will be the post the comment belongs to.
@@ -16,17 +17,28 @@ class Comment{
   protected $commentsData = array();
 
   public function __construct(){
+    //Get an instance of the DatabaseConnection class.
     $this->DatabaseConnection = DatabaseConnection::getInstance();
+    //Create an instance of the ErrorHandler class so we can log errors.
     $this->ErrorHandler = new ErrorHandler("Comment");
     //Create an instance of the Validator class so we can validate the commentText so it is more secure.
     $this->Validator = new Validator();
   }
+
+
   public function getComments($postID){ 
-    date_default_timezone_set('Europe/London'); //will have to change this timezone if there are international users of the app.
+    /*
+     * The purpose of this method is to get all comments (from the database) of a particular post with an ID of postID.
+     * We will not need the full user profile data for comments so we leave out certain properties like bio and coverImgUrl etc.
+     */
+    //Define the current timezone.
+    //Note: we will have to change this timezone if there are international users of the app.
+    date_default_timezone_set('Europe/London'); 
     $this->postID = $postID;
+    $commentsData = array();
     foreach($this->DatabaseConnection->getComments($postID) as $key => $value){  
       array_push(
-        $this->commentsData, array(
+        $commentsData, array(
           'commentID' => $value['commentID'],
           'commentText' => $this->Validator->cleanUserInput($value['commentText']),
           'commentTime' => date("jS F Y H:i", $value['commentTime']), 
@@ -35,14 +47,16 @@ class Comment{
         )
       ); 
     }
-    return $this->commentsData;
+    return $commentsData;
   }
+
+
   public function getCommentUserData($commentUserID){ 
     /*
-     * The purpose of this method is to preapre the user data associated with a comment on a post.
+     * The purpose of this method is to prepare the user data associated with a comment on a post.
      * We will not need the full user profile data for comments so we leave out certain properties like bio and coverImgUrl etc.
      */
-    //get all the user profile data.
+    //get all the user profile data for a particular comment.
     $userProfileData = $this->DatabaseConnection->getUserProfile($commentUserID);
     //only store the data that we need for the comments.
     $this->commentUserData['userID'] = $userProfileData['userID'];
